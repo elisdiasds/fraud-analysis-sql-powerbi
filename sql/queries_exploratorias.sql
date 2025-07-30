d6922d2d24dde1fa0ea7d12d89e6557272d01e39
Elisângela Dias <elisdiasds@gmail.com>
2025-07-30 16:27:43 -0300

    Add files via upload

---
+++ sql/queries_exploratorias.sql
@@ -0,0 +1,25 @@
+-- Total de transações
+SELECT Count(*) FROM sample_creditcard;
+
+-- Total de fraudes
+SELECT COUNT(*) FROM sample_creditcard where class= 1;
+
+-- Taxa de fraude 
+SELECT ROUND(SUM(Class) / COUNT(*) * 100,2) AS taxa_fraude_percentual FROM sample_creditcard;
+
+-- Valor Total da fraude
+
+SELECT SUM(Amount) FROM sample_creditcard WHERE Class=1;
+-- Distribuição de fraudes na primeira hora
+
+SELECT 
+  FLOOR(Time / 60) AS minuto,
+  COUNT(*) AS total_transacoes,
+  SUM(Class) AS total_fraudes
+FROM sample_creditcard
+GROUP BY minuto
+ORDER BY minuto
+LIMIT 60;
+
+
+
